a8c30f8895c812a94a860e04eee0963c671d40c9
pc <a@mail>
2023-10-07 10:18:33 +0500

    init

---
+++ bin/secondwork.dart
@@ -0,0 +1,5 @@
+import 'package:secondwork/secondwork.dart' as secondwork;
+
+void main(List<String> arguments) {
+  print('Hello world: ${secondwork.calculate()}!');
+}
---
+++ test/secondwork_test.dart
@@ -0,0 +1,8 @@
+import 'package:secondwork/secondwork.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
